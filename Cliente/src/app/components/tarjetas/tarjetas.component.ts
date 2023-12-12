c0cb237ed8a27a3b34feb587654c6b61686e7a70
MatiasColazo1 <maty_colazo@hotmail.com>
2023-12-12 05:33:33 -0300

    modal

--- Cliente/src/app/components/tarjetas/tarjetas.component.ts
+++ Cliente/src/app/components/tarjetas/tarjetas.component.ts
@@ -1,7 +1,9 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DarkModeService } from 'src/app/services/dark-mode.service';
 import { TarjetasService } from 'src/app/services/tarjetas.service';
+
 
 @Component({
   selector: 'app-tarjetas',
@@ -9,6 +11,8 @@ import { TarjetasService } from 'src/app/services/tarjetas.service';
   styleUrls: ['./tarjetas.component.css']
 })
 export class TarjetasComponent implements OnInit {
+ 
+  @ViewChild('modal', { static: true }) modalAbrir!: ElementRef
   tarjetas: any[] = [];
   nuevaTarjeta: any = {
     titulo:'',
@@ -16,33 +20,32 @@ export class TarjetasComponent implements OnInit {
   }
   tarjetaActiva: any = null;
 
-
-  constructor(private tarjetasService: TarjetasService, public darkModeService: DarkModeService) { }
-
+  constructor(private tarjetasService: TarjetasService, public darkModeService: DarkModeService, private modalService: NgbModal) { }
+  
   ngOnInit(): void {
     this.getTarjetas()
   }
-
+  
   getTarjetas() {
     this.tarjetasService.getTarjeta().subscribe((data: any) => {
       this.tarjetas = data.map((tarjeta: any) => ({ ...tarjeta, expandida: false }));
     })
   }
-
-
+  
+  
   postTarjeta() {
     this.tarjetasService.postTarjeta(this.nuevaTarjeta).subscribe(
-    (data: any) => {
-      console.log('Tarjeta agregada con exito: ', data);
-      this.getTarjetas();
-      this.nuevaTarjeta = {}
-    },
-    (error) => {
-      console.error('Error al agregar tarjeta: ', error);
+      (data: any) => {
+        console.log('Tarjeta agregada con exito: ', data);
+        this.getTarjetas();
+        this.nuevaTarjeta = {}
+      },
+      (error) => {
+        console.error('Error al agregar tarjeta: ', error);
+      }
+      )
     }
-    )
-  }
-
+    
   toggleTarjeta(tarjeta: any) {
     // Cambiar el estado expandido al hacer clic en la tarjeta
     if (this.tarjetaActiva !== tarjeta) {
@@ -59,5 +62,4 @@ export class TarjetasComponent implements OnInit {
     this.tarjetaActiva = tarjeta;
   }
 
-  
 }
