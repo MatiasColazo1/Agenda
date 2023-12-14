ef1d15df20f0526911bc1ef864d5d773fecf13e8
MatiasColazo1 <maty_colazo@hotmail.com>
2023-12-14 19:13:50 -0300

    Notas

--- Cliente/src/app/components/notas/notas.component.ts
+++ Cliente/src/app/components/notas/notas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-notas',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./notas.component.css']
 })
 export class NotasComponent {
+  @ViewChild('textareaElement') textareaElement!: ElementRef;
 
+  
+
+  copy() {
+    // Obtener el texto del textarea
+    const textareaValue = this.textareaElement.nativeElement.value;
+  
+    // Copiar al portapapeles
+    const el = document.createElement('textarea');
+    el.value = textareaValue;
+    document.body.appendChild(el);
+    
+    // Esperar a que Angular actualice la vista
+    setTimeout(() => {
+      // Seleccionar todo el texto del textarea después de un pequeño retraso
+      this.textareaElement.nativeElement.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+  
+      // Puedes mostrar un mensaje de éxito o realizar otras acciones después de copiar.
+      console.log('Texto copiado al portapapeles:', textareaValue);
+    });
+}
 }
