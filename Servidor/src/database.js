103a7aaddfb600c80b18cbdf640b083c10264231
MatiasColazo1 <maty_colazo@hotmail.com>
2024-01-08 23:07:02 -0300

    env database

--- Servidor/src/database.js
+++ Servidor/src/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://maticolazo97:Z3uCB4kcccB4Iqtn@cluster0.twmgqtm.mongodb.net/?retryWrites=true&w=majority', {
-     
-})
-    .then(db => console.log('Database conectado'))
-    .catch(err => console.log(err));
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.twmgqtm.mongodb.net/?retryWrites=true&w=majority`)
+   .then(db => console.log('Database conectado'))
+   .catch(err => console.log(err));
