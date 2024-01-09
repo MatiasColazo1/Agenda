f9ef1e4aae4220a4156ba802960fb6b96383e2e5
MatiasColazo1 <maty_colazo@hotmail.com>
2024-01-08 23:11:00 -0300

    .env

--- Servidor/src/database.js
+++ Servidor/src/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-
+//Mongo Atlas deploy
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.twmgqtm.mongodb.net/?retryWrites=true&w=majority`)
    .then(db => console.log('Database conectado'))
    .catch(err => console.log(err));
