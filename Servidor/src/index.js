fb7f399e6ceceaed3f7e70b827ec9adf3e59036e
MatiasColazo1 <maty_colazo@hotmail.com>
2024-01-09 21:20:12 -0300

    cors

--- Servidor/src/index.js
+++ Servidor/src/index.js
@@ -3,10 +3,8 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const cors = require('cors');
 
 
-app.use(cors());
 
 
 const { google } = require('googleapis');
